9f62febaac996939b6c9bf81164d804490150150
murkrow <marcocoppola02.mc@gmail.com>
2024-07-12 16:14:26 +0200

    Shopping list

--- widget.py
+++ widget.py
@@ -1,41 +1,19 @@
-from mosaico import widget
+from mosaico import widget, config
 
-# A nice rectangle
-rectTop = widget.createRectangle()
-rectTop.translateY(10)
-rectTop.translateX(-rectTop.getWidth())
-
-# Another nice rectangle
-rectTop2 = widget.createRectangle()
-rectTop2.translateY(10)
-rectTop2.translateX(64+rectTop2.getWidth())
-
-# A nice rectangle
-rectBottom = widget.createRectangle()
-rectBottom.translateY(40)
-rectBottom.translateX(-rectBottom.getWidth())
-
-# Another nice rectangle
-rectBottom2 = widget.createRectangle()
-rectBottom2.translateY(40)
-rectBottom2.translateX(64+rectBottom2.getWidth())
-
-# A nice text
+# Create title
 text = widget.createText()
-text.setText("GGGGGG")
-text.translateX(10)
-text.translateY(25)
+text.setText(config["name"])
+text.setHexColor(config["color"])
+text.translate(2,2)
+text.setFontHeight(10)
 
+# Create items
+items = []
+for i in range(0, len(config["items"])):
+    items.append(widget.createText())
+    items[i].setFontHeight(6)
+    items[i].setText(config["items"][i])
+    items[i].translate(2, 6+ 7 * (i + 1))
 
-i = 0
 def loop():
-    global i
-    i += 1
-    if i > 70:
-        i = 0
-    rectTop.moveTo(i, rectTop.getY())
-    rectTop2.moveTo(63-i-rectTop2.getWidth(), rectTop2.getY())
-    rectBottom.moveTo(i, rectBottom.getY())
-    rectBottom2.moveTo(63-i-rectBottom2.getWidth(), rectBottom2.getY())
     pass
-        
